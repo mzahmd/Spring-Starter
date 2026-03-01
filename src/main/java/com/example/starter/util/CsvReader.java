30e8b78d22c5ac0ec86a01d25cf6180ac3ee1a05
mahme001 <minhaz.ahmed@student.hs-rm.de>
2026-03-01 23:46:13 +0100

    test authenticationcontroller

--- src/main/java/com/example/starter/util/CsvReader.java
+++ src/main/java/com/example/starter/util/CsvReader.java
@@ -1,33 +1,33 @@
-package com.example.starter.util;
-
-import com.example.starter.model.Author;
-import com.opencsv.CSVReader;
-import org.springframework.stereotype.Component;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
-
-@Component
-public class CsvReader {
-
-    public List<Author> readPersons(InputStream inputStream) throws Exception {
-        List<Author> persons = new ArrayList<>();
-
-        try (CSVReader reader = new CSVReader(new InputStreamReader(inputStream))) {
-            String[] line;
-            reader.readNext();
-
-            while ((line = reader.readNext()) != null) {
-                Author author = new Author();
-                author.setName(line[0]);
-                author.setEmail(line[1]);
-                author.setAge(Integer.parseInt(line[2]));
-                persons.add(author);
-            }
-        }
-        return persons;
-    }
-}
+//package com.example.starter.util;
+//
+//import com.example.starter.model.Author;
+//import com.opencsv.CSVReader;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//
+//@Component
+//public class CsvReader {
+//
+//    public List<Author> readPersons(InputStream inputStream) throws Exception {
+//        List<Author> persons = new ArrayList<>();
+//
+//        try (CSVReader reader = new CSVReader(new InputStreamReader(inputStream))) {
+//            String[] line;
+//            reader.readNext();
+//
+//            while ((line = reader.readNext()) != null) {
+//                Author author = new Author();
+//                author.setName(line[0]);
+//                author.setEmail(line[1]);
+//                author.setAge(Integer.parseInt(line[2]));
+//                persons.add(author);
+//            }
+//        }
+//        return persons;
+//    }
+//}
--- src/test/java/com/example/starter/Authentication/AuthenticationControllerTest.java
+++ src/test/java/com/example/starter/Authentication/AuthenticationControllerTest.java
@@ -1,10 +1,29 @@
 package com.example.starter.Authentication;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
+//@ExtendWith(MockitoExtension.class)
 class AuthenticationControllerTest {
 
+    // @Mock => kein JUnit test durch Mockito
+    // @Autowired => geht nur wenn der Spring Context gestartet wird
+    AuthenticationController authenticationController = new AuthenticationController();
+
+
     @Test
-    void login() {
+    void login() throws Exception {
+        // Given
+        AuthenticationRequestDTO dto = new AuthenticationRequestDTO("test@test.com", "password");
+
+        // When
+        ResponseEntity<String> res = authenticationController.login(dto);
+
+        // Then
+        Assertions.assertEquals(HttpStatus.OK, res.getStatusCode());
+        Assertions.assertEquals("User is valid", res.getBody());
+
     }
 }
