28e70bc951459268c4aee9feeb794b1fda148a90
mahme001 <minhaz.ahmed@student.hs-rm.de>
2026-04-26 11:37:24 +0200

    constraint

--- src/main/java/com/example/starter/Authentication/AuthenticationRequestDTO.java
+++ src/main/java/com/example/starter/Authentication/AuthenticationRequestDTO.java
@@ -2,11 +2,16 @@ package com.example.starter.Authentication;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record AuthenticationRequestDTO(
 
         @NotBlank(message = "Email field is empty")
-        @Email(message = "Email field is not correct")
+//        @Email(message = "Email field is not correct")
+        @Pattern(
+                regexp = "^[A-Za-z0-9._%+-]+@gmail\\.com$",
+                message = "Email field is not correct"
+        )
         String email,
 
         @NotBlank
